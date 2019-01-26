31f0128760f400d04f478355203e980723f9e3e0
Noel-96 <noelorion69@gmail.com>
2019-01-26 07:40:45 +0100

    chore :  set up API model for more Top rated movies

--- app/src/main/java/com/noel_inc/android_mvp_demo_ii/network/retrofitclient/api_for_movie_name/MovieApiService.java
+++ app/src/main/java/com/noel_inc/android_mvp_demo_ii/network/retrofitclient/api_for_movie_name/MovieApiService.java
@@ -1,5 +1,7 @@
 package com.noel_inc.android_mvp_demo_ii.network.retrofitclient.api_for_movie_name;
 
+import com.noel_inc.android_mvp_demo_ii.network.apimodel.get_topmovies_api.TopRated;
+
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
